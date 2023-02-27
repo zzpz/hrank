855081d6bdf18d7567076eb070476e4f9635084c
zzpz <michael.wilson1@uqconnect.edu.au>
2023-02-27 18:54:14 +1000

    a.map() + new Map()

---
+++ prep/TS/8-LonelyInt.ts
@@ -0,0 +1,63 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a: number[]): number {
+    // Write your code here
+    
+    //just use a map and some laziness
+    
+    const map = new Map()
+    a.map((value)=>{
+        if(map.has(value)){
+            map.delete(value)
+        }else{
+            map.set(value,null)
+        }
+    })
+    
+    return map.keys().next().value
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const a: number[] = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result: number = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
